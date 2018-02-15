15d2f8b19afee779d71928621d3910a196ab610b
lmusolino <l.musolino@trim.it>
2018-02-15 10:05:29 +0100

    Aggiunto docker-compose per docker swarm

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --restart=always -d --network sviluppo --name jenkins -v ${VOLUME_DIR}jenkins:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins
+docker run --restart=always -d --network net --name jenkins -v ${VOLUME_DIR}jenkins:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins
