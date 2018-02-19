802c952d82175ed14c39c6091627096968ca5bb0
lmusolino <l.musolino@trim.it>
2018-02-19 19:07:38 +0100

    build

--- traefik/build.sh
+++ traefik/build.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-docker-compose -p traefik down
-docker-compose -p traefik -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -p traefik -f docker-compose.yml -f docker-compose.prod.yml up -d --build
